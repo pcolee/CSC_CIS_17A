915a9f6d0deaa9f4c1b1974436c167b5147e8c85
pcolee <kcolwa@gmail.com>
2025-09-08 02:39:33 -0700

    Add IntroLab and Lab Conversion Table HW

---
+++ Hmwrk/IntroLab/Hello World .cpp
@@ -0,0 +1,19 @@
+/*
+Name: Keisha Coleen (Cole) Wagas
+Date: 09/03/2025   
+Purpose: Hello World
+*/
+
+//System Libraries
+#include <iostream> 
+using namespace std;
+
+//Function Prototypes
+int main(int argv, char **argc) {
+
+    //Display and output the results
+    cout << "\nHello World!\n\n";
+
+    //Exit the program
+    return 0;
+}
